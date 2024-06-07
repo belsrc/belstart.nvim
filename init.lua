0bf1759d79f831a315ebf4de4c1255f09ec8c1bd
belsrc <brykizer@gmail.com>
2024-06-06 23:18:04 -0400

    feat: add keymap for neotree

--- init.lua
+++ init.lua
@@ -873,8 +873,10 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+  require 'kickstart.plugins.neo-tree',
+  vim.keymap.set('n', '<leader>tt', '<cmd>Neotree toggle<cr>', { desc = '[T]oggle Neo[T]ree' }),
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
