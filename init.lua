c12c4021e19b37355a5d334771644258c39a220f
belsrc <brykizer@gmail.com>
2024-06-08 15:00:21 -0400

    feat: add title screen

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 require 'mappings'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
@@ -136,6 +135,70 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+  },
+
+  {
+    'goolord/alpha-nvim',
+    event = 'VimEnter',
+    config = function()
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
+
+      -- Header
+      dashboard.section.header.val = {
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                     ]],
+        [[       ████ ██████           █████      ██                     ]],
+        [[      ███████████             █████                             ]],
+        [[      █████████ ███████████████████ ███   ███████████   ]],
+        [[     █████████  ███    █████████████ █████ ██████████████   ]],
+        [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+        [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+        [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+      }
+
+      _Gopts = {
+        position = 'center',
+        hl = 'Type',
+        -- wrap = "overflow";
+      }
+
+      -- Set Menu
+      dashboard.section.buttons.val = {
+        -- dashboard.button("SPC j", "   Restore Session", "<cmd>SessionRestore<cr>"),
+        dashboard.button('e', '   New file', '<cmd>ene<CR>'),
+        dashboard.button('r', '   Recent', '<cmd>Telescope oldfiles<CR>'),
+        dashboard.button('f', '   Find file', '<cmd>Telescope find_files<CR>'),
+        dashboard.button('p', '   Projects', '<cmd>Telescope projects<CR>'),
+        dashboard.button('c', '   Config', '<cmd>e $MYVIMRC <CR>'),
+        dashboard.button('m', '󱌣   Mason', '<cmd>Mason<CR>'),
+        dashboard.button('l', '󰒲   Lazy', '<cmd>Lazy<CR>'),
+        dashboard.button('u', '󰂖   Update plugins', "<cmd>lua require('lazy').sync()<CR>"),
+        dashboard.button('q', '   Quit NVIM', '<cmd>qa<CR>'),
+      }
+
+      -- Footer load times
+      vim.api.nvim_create_autocmd('User', {
+        callback = function()
+          local stats = require('lazy').stats()
+          local ms = math.floor(stats.startuptime * 100) / 100
+          dashboard.section.footer.val = '󱐌 Lazy-loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
+
+      dashboard.opts.opts.noautocmd = true
+      alpha.setup(dashboard.opts)
+    end,
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
