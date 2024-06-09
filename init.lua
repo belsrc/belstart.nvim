260485d39b4593d081be6ad4e882e259a4841694
belsrc <brykizer@gmail.com>
2024-06-09 17:25:55 -0400

    refactor: change git gutter icons

--- init.lua
+++ init.lua
@@ -112,11 +112,9 @@ require('lazy').setup({
   -- keys can be used to configure plugin behavior/loading/etc.
   --
   -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
   --    require('Comment').setup({})
+  --    require('gitsigns').setup({ ... })
 
-  -- "gc" to comment visual regions/lines
   {
     'numToStr/Comment.nvim',
     opts = {
@@ -131,20 +129,15 @@ require('lazy').setup({
     },
   },
 
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = { text = '' },
+        change = { text = '' },
+        delete = { text = '' },
+        topdelete = { text = '' },
+        changedelete = { text = '' },
       },
     },
   },
