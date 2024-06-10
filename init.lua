53bf63a64ede78c2b7bc12a97751b57999d18ddf
Bryan Kizer <brykizer@gmail.com>
2024-06-10 09:17:31 -0400

    feat: add ts-tools plugin

--- init.lua
+++ init.lua
@@ -474,6 +474,16 @@ require('lazy').setup({
     end,
   },
 
+  { -- Typesctipt tooling
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {
+      expose_as_code_action = 'all',
+      complete_function_calls = true,
+      include_completions_with_insert_text = true,
+    },
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
